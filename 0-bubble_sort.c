03d9c245c37be57756c9adca28b1a85fe61452b6
layan2k <lesliedeeshumba@gmail.com>
2021-10-24 16:02:49 +0200

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * Bubble sort is a simple sorting algorithm.
+ * This sorting algorithm is comparison-based algorithm in which each pair
+ * of adjacent elements is compared and the elements are swapped if they are not in order.
+ *
+ * @array: input array.
+ * @size: size of the array.
+ * Return: no return
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, n;
+	int tmp, exchange;
+
+	for (n = size, exchange = 1; n > 0 && exchange; n--)
+	{
+		exchange = 0;
+		for (i = 0; (i + 1) < n; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = tmp;
+				print_array(array, size);
+				exchange = 1;
+			}
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -1,6 +1,27 @@
 #ifndef _SORT_
 #define _SORT_
 
+// import libraries
+
 #include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * struct listint_dub - Doubly linked list node
+ *
+ * @n: Integer
+ * @nxt: Pointer to the next element of the list.
+ * @prv: Pointer to the previous element of the list.
+ */
+typedef struct listint_dub
+{
+	const int n;
+	struct listint_dub *prv;
+	struct listint_dub *nxt;
+} listint_t;
+
+
+// Prototypes
+void bubble_sort(int *array, size_t size);
 
 #endif
